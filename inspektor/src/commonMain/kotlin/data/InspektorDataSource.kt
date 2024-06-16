70ff507075a48ca041b3f4643ac054e8c1c5fbd2
Shreyash Kore <kore.shreyash@gmail.com>
2024-06-16 20:08:24 +0530

    update InspektorDataSource

---
+++ inspektor/src/commonMain/kotlin/data/InspektorDataSource.kt
@@ -0,0 +1,45 @@
+package data
+
+import com.gyanoba.inspektor.data.entites.HttpTransaction
+import data.db.createDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+import kotlinx.datetime.Instant
+
+
+internal class InspektorDataSource private constructor() {
+    private val db = createDatabase()
+
+    suspend fun insertHttpTransaction(httpTransaction: HttpTransaction) =
+        withContext(Dispatchers.IO) {
+            db.httpTransactionQueries.insert(httpTransaction)
+        }
+
+    suspend fun updateHttpTransaction(httpTransaction: HttpTransaction) =
+        withContext(Dispatchers.IO) {
+            db.httpTransactionQueries.update(
+                httpTransaction.responseCode,
+                httpTransaction.responseBody,
+                httpTransaction.responsePayloadSize,
+                httpTransaction.responseContentType,
+                httpTransaction.responseHeaders,
+                httpTransaction.responseHeadersSize,
+                httpTransaction.responseBody,
+                httpTransaction.isResponseBodyEncoded,
+                httpTransaction.id,
+            )
+        }
+
+    suspend fun getAllLatestHttpTransactions() = withContext(Dispatchers.IO) {
+        db.httpTransactionQueries.getAllLatest().executeAsList()
+    }
+
+    suspend fun deleteBefore(timestamp: Instant) = withContext(Dispatchers.IO) {
+        db.httpTransactionQueries.deleteBefore(timestamp)
+    }
+
+    companion object {
+        val Instance by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) { InspektorDataSource() }
+    }
+}
--- inspektor/src/commonMain/kotlin/data/db/Db.kt
+++ inspektor/src/commonMain/kotlin/data/db/Db.kt
@@ -5,7 +5,7 @@ import com.gyanoba.inspektor.data.entites.HttpTransaction
 import com.gyanoba.inspektor.db.Database
 import data.adapters.instantAdapter
 
-internal const val DB_NAME = "inspektor.db"
+internal const val DB_NAME = "com.gyanoba.inspektor.db"
 
 internal expect object DriverFactory {
     fun createDbDriver(): SqlDriver
