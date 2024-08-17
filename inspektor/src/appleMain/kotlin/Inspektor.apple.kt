353dcb0abaafc58520fc3913934ed07eafc1f86c
Shreyash Kore <kore.shreyash@gmail.com>
2024-08-18 00:35:48 +0530

    add ios sample and openInspektor implementation

--- inspektor/src/appleMain/kotlin/Inspektor.apple.kt
+++ inspektor/src/appleMain/kotlin/Inspektor.apple.kt
@@ -1,2 +1,10 @@
+import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIApplication
+import ui.App
+
 public actual fun openInspektor() {
+    val pluginViewController = ComposeUIViewController { App() }
+    val topController = UIApplication.sharedApplication.keyWindow?.rootViewController
+        ?: throw IllegalStateException("No key window or root view controller found")
+    topController.presentViewController(pluginViewController, animated = true, completion = null)
 }
---
+++ iosApp/Inspektor Sample/Inspektor_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inspektor_SampleApp.swift
+//  Inspektor Sample
+//
+//  Created by Shreyash Kore on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Inspektor_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
