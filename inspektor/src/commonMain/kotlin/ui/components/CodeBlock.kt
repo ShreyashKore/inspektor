7ca6b26bb59fb8f69fc09917484d920b5a2173ce
Shreyash Kore <kore.shreyash@gmail.com>
2024-08-26 03:28:07 +0530

    add SearchableText and change CodeBlock to use it

--- inspektor/src/commonMain/kotlin/ui/components/CodeBlock.kt
+++ inspektor/src/commonMain/kotlin/ui/components/CodeBlock.kt
@@ -1,23 +1,20 @@
 package ui.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,14 +23,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.dp
 import com.gyanoba.inspektor.inspektor.generated.resources.Res
 import com.gyanoba.inspektor.inspektor.generated.resources.round_content_copy
 import com.gyanoba.inspektor.inspektor.generated.resources.round_done
+import com.gyanoba.inspektor.inspektor.generated.resources.wrap_text
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
@@ -45,67 +42,106 @@ internal fun CodeBlock(
     modifier: Modifier = Modifier,
 ) {
     val clipboardManager = LocalClipboardManager.current
+
     val scope = rememberCoroutineScope()
-    val showCopyButton = remember { mutableStateOf(true) }
-    var showCopiedIndication by remember { mutableStateOf(false) }
-    val vScrollState = rememberScrollState()
-    val hScrollState = rememberScrollState()
-    Box(modifier = modifier) {
-        SelectionContainer(
-            Modifier
-                .pointerInput(Unit) {
-                    awaitEachGesture {
-                        awaitFirstDown()
-                        showCopyButton.value = false
-                        do {
-                            val event = awaitPointerEvent()
-                        } while (event.changes.any { it.pressed })
-                        showCopyButton.value = true
+    val searchableTextState = remember {
+        SearchableTextState(initialText = code, scope)
+    }
+
+    var showSearch by remember { mutableStateOf(false) }
+    var softWrap by remember { mutableStateOf(false) }
+
+    Column(modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Spacer(Modifier.weight(1f))
+            CopyButton(
+                onClick = {
+                    clipboardManager.setText(code)
+                },
+                contentDescription = "Copy All"
+            )
+            IconToggleButton(
+                checked = softWrap,
+                onCheckedChange = { softWrap = it },
+            ) {
+                Icon(
+                    painterResource(Res.drawable.wrap_text),
+                    contentDescription = "Wrap Text"
+                )
+            }
+            IconToggleButton(
+                checked = showSearch,
+                onCheckedChange = { showSearch = it },
+            ) {
+                AnimatedContent(
+                    targetState = showSearch,
+                ) { showingSearch ->
+                    if (showingSearch) {
+                        Icon(
+                            Icons.Rounded.Close,
+                            contentDescription = "Close"
+                        )
+                    } else {
+                        Icon(
+                            Icons.Rounded.Search,
+                            contentDescription = "Search"
+                        )
                     }
                 }
-        ) {
-            Text(
-                code,
-                style = MaterialTheme.typography.bodySmall.copy(fontFamily = FontFamily.Monospace),
-                softWrap = true,
-                modifier = Modifier.fillMaxSize().padding(8.dp)
-                    .verticalScrollbar(vScrollState)
-                    .horizontalScrollbar(hScrollState)
-                    .verticalScroll(vScrollState)
-                    .horizontalScroll(hScrollState)
+            }
+
+        }
+        AnimatedVisibility(showSearch) {
+            SearchTextToolbar(
+                searchableTextState = searchableTextState
             )
         }
 
-
-        AnimatedVisibility(
-            true,
-            enter = fadeIn(tween(delayMillis = 1000)),
-            exit = fadeOut(),
-            modifier = Modifier.align(Alignment.TopEnd)
-        ) {
-            IconButton(
-                onClick = {
-                    clipboardManager.setText(code)
-                    scope.launch {
-                        showCopiedIndication = true
-                        delay(1500)
-                        showCopiedIndication = false
-                    }
-                },
-                colors = IconButtonDefaults.iconButtonColors(
-                    contentColor = if (showCopiedIndication) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSurface,
-                    containerColor = if (showCopiedIndication) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-                ),
-            ) {
-                Icon(
-                    painterResource(
-                        if (showCopiedIndication) Res.drawable.round_done else
-                            Res.drawable.round_content_copy
-                    ),
-                    contentDescription = "Copy All"
-                )
-            }
+        SelectionContainer {
+            SearchableText(
+                searchState = searchableTextState,
+                style = MaterialTheme.typography.bodyMedium.copy(fontFamily = FontFamily.Monospace),
+                modifier = Modifier.fillMaxWidth(),
+                softWrap = softWrap,
+                showLineNumbers = true
+            )
         }
+    }
+}
+
+
+@Composable
+internal fun CopyButton(
+    onClick: () -> Unit,
+    contentDescription: String = "Copy",
+    modifier: Modifier = Modifier,
+) {
+    var showCopiedIndication by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    IconButton(
+        modifier = modifier,
+        onClick = {
+            onClick()
+            scope.launch {
+                showCopiedIndication = true
+                delay(1500)
+                showCopiedIndication = false
+            }
+        },
+        colors = IconButtonDefaults.iconButtonColors(
+            contentColor = if (showCopiedIndication) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSurface,
+            containerColor = if (showCopiedIndication) MaterialTheme.colorScheme.primary else Color.Unspecified,
+        ),
+    ) {
+        Icon(
+            painterResource(
+                if (showCopiedIndication) Res.drawable.round_done else
+                    Res.drawable.round_content_copy
+            ),
+            contentDescription = contentDescription
+        )
     }
 }
 
